4ed22ac060725606d841bfd922b999519a813dbe
gps57 <glen@swiftbt.com>
2021-01-06 16:00:05 -0500

    End of section 14

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,6 +10,34 @@ namespace API.Data
     }
 
     public DbSet<AppUser> Users { get; set; }
+    public DbSet<UserLike> Likes { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      base.OnModelCreating(builder);
+
+      builder.Entity<UserLike>()
+        .HasKey(k => new {k.SourceUserId, k.LikedUserId});
+
+      builder.Entity<UserLike>()
+        .HasOne(s => s.SourceUser)
+        .WithMany(l => l.LikedUsers)
+        .HasForeignKey(s => s.SourceUserId)
+        // Here's a NOTE from course material...
+        // If using SQL Server then will need to set DeleteBehavior here to
+        // DeleteBehavior.NoAction
+        // ... or will get an error during migration
+        .OnDelete(DeleteBehavior.Cascade);
+
+      builder.Entity<UserLike>()
+        .HasOne(s => s.LikedUser)
+        .WithMany(l => l.LikedByUsers)
+        .HasForeignKey(s => s.LikedUserId)
+        // Here's a NOTE from course material...
+        // If using SQL Server then will need to set DeleteBehavior here to
+        // DeleteBehavior.NoAction
+        // ... or will get an error during migration
+        .OnDelete(DeleteBehavior.Cascade);
+    }
   }
 }
